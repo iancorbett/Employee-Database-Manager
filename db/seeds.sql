0381f4d998fbf1ce48f3dae9bc3a9f210e8f3fa4
iancorbett <iancorbett1324@gmail.com>
2024-10-21 17:37:46 -0600

    Update seeds.sql
    editing seeds.sql to match walkthrough video

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,21 +5,24 @@ VALUES ( 'Human Resources'),
        ( 'Engineering'),
        ( 'Management'),
        ( 'Accounting'),
+       ( 'Security'),
        ( 'Interns');
 
 
 
-INSERT INTO roles (role_id, role_title, role_department, role_salary)
-VALUES (1, 'Software Engineer', 'Engineering', 100000),
-       (2, 'Accountant', 'Accounting', 90000),
-       (3, 'Manager', 'Management', 150000),
-       (4, 'Chef', 'Cafeteria', 60000);
+INSERT INTO roles (role_name, role_department, role_salary)
+VALUES ( 'Software Engineer', 'Engineering', 100000),
+       ( 'Accountant', 'Accounting', 90000),
+       ( 'Manager', 'Management', 150000),
+       ( 'Chef', 'Cafeteria', 60000),
+       ( 'Police Officer', 'Security', 75000);
 
-INSERT INTO employees (employee_id, employee_firstName, employee_lastName, employee_jobTitle, employee_department, employee_salary, employee_managers)
-VALUES (1, 'James', 'Smith', 'Software Engineer', 'Engineering', 100000, 'John'),
-       (2, 'Mark', 'Johnson', 'Accountant', 'Accounting', 90000, 'Samuel'),
-       (3, 'Justin', 'Walker', 'Manager', 'Management', 150000, 'Robert'),
-       (4, 'Michael', 'Williams', 'Chef', 'Cafeteria', 60000, 'Quinton');
+INSERT INTO employees (employee_firstName, employee_lastName, employee_jobTitle, employee_department, employee_salary, employee_managers)
+VALUES ( 'James', 'Smith', 'Software Engineer', 'Engineering', 100000, 'John'),
+       ( 'Mark', 'Johnson', 'Accountant', 'Accounting', 90000, 'Samuel'),
+       ( 'Justin', 'Walker', 'Manager', 'Management', 150000, 'Robert'),
+       ( 'Michael', 'Williams', 'Chef', 'Cafeteria', 60000, 'Quinton'),
+       ( 'Jenson', 'Jones', 'HR Specialist', 'Human Resources', 65000, 'Richard');
 
 SELECT * FROM departments;
 SELECT * FROM roles;
