703dc9fc1ce2763fd21feb51329d305404452703
iancorbett <iancorbett1324@gmail.com>
2024-10-21 17:39:09 -0600

    Update schema.sql
    editing schema.sql to match walkthrough video

--- db/schema.sql
+++ db/schema.sql
@@ -9,18 +9,19 @@ CREATE TABLE departments(
 );
 
 CREATE TABLE roles(
-    role_id INTEGER NOT NULL,
-    role_title VARCHAR(30) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    role_name VARCHAR(30) NOT NULL,
     role_department VARCHAR(30) NOT NULL,
     role_salary INTEGER NOT NULL
 );
 
 CREATE TABLE employees(
-    employee_id INTEGER NOT NULL,
+    employee_id SERIAL PRIMARY KEY,
     employee_firstName VARCHAR(30) NOT NULL,
     employee_lastName VARCHAR(30) NOT NULL,
-    employee_jobTitle VARCHAR(30) NOT NULL,
+    employee_jobTitle VARCHAR(30) NOT NULL, --FOREIGN KEY REFERS TO ROLE
+    
     employee_department VARCHAR(30) NOT NULL,
     employee_salary INTEGER NOT NULL,
-    employee_managers VARCHAR(30) NOT NULL
+    employee_managers VARCHAR(30) NOT NULL --FOREIGN KEY REFERS TO EMPLOYEE TABLE
 );
