ab1b49cc30fd25cc751534ff12a7a791ec1a2069
iancorbett <iancorbett1324@gmail.com>
2024-10-21 17:40:33 -0600

    Update index.js
    edit index.js to match walkthrough video

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ inquirer
             
              pool.query('INSERT INTO departments (department_name) VALUES ($1)', [answers.departmentName])
             }
-            
+            console.log("Data Inserted")
         })
     }
     else if (answers.showTable === "Add a role") {
@@ -70,16 +70,27 @@ inquirer
         .prompt ([
             {
                 type: "input",
-                message: "Enter a new role here",
+                message: "Enter a new role title here",
                 name: "roleName"
+            },
+            {
+                type: "input",
+                message: "Enter the role's department",
+                name: "roleDepartment"
+            },
+            {
+                type: "input",
+                message: "Enter the role's salary",
+                name: "roleSalary"
             }
         ])
         .then((answers) => {
             if (answers.roleName !== "") {
                 console.log(answers.roleName);
                 //FIX SO IT ADDS USER INPUT TO ROLES TABLE
-                pool.query()
+                // pool.query('INSERT INTO roles (role_name, role_department, role_salary) VALUES ($1)', [answers.roleName], [answers.roleDepartment], [answers.roleSalary])
             }
+            console.log('Data Inserted')
         })
     }
     else if (answers.showTable === "Add an employee") {
@@ -87,30 +98,72 @@ inquirer
         .prompt ([
             {
                 type: "input",
-                message: "Enter a new employee here",
-                name: "employeeName"
+                message: "Enter the new employee's first name here",
+                name: "employeeFirstName"
+            },
+            {
+                type: "input",
+                message: "Enter the new employee's last name here",
+                name: "employeeLastName"
+            },
+            {
+                type: "input",
+                message: "Enter the new employee's job title here",
+                name: "employeeJobTitle"
+            },
+            {
+                type: "input",
+                message: "Enter the new employee's department here",
+                name: "employeeDepartment"
+            },
+            {
+                type: "input",
+                message: "Enter the new employee's salary here",
+                name: "employeeSalary"
+            },
+            {
+                type: "input",
+                message: "Enter the new employee's manager here",
+                name: "employeeManager"
             }
         ])
         .then((answers) => {
-            console.log(answers.employeeName);
-            //FIX SO IT ADDS USER INPUT TO EMPLOYEES TABLE
-            pool.query()
-        })
+            //console.log(answers.employeeName);
+            
+        //      pool.query('INSERT INTO employees (employee_firstName, employee_lastName, employee_jobTitle, employee_department, employee_salary, employee_manager) VALUES ($1)', [answers.employeeFirstName], [answers.employeeLastName], [answers.employeeJobTitle], [answers.employeeDepartment], [answers.employeeSalary], [answers.employeeManager])
+        console.log('Data Inserted');     
+    });
+        
     }
-    else if (answers.showTable === "Update an employee role") {
+    else if (answers.showTable === "Update employee role") {
         inquirer.prompt ([
             {
                 type: "input",
-                message: "Update the employee role here",
+                message: "What is the employee's name?",
+                name: "employeeName"
+            },
+            {
+                type: "input",
+                message: "What is the employee's current Position?",
                 name: "updateRole"
+            },
+            {
+                type: "input",
+                message: "What will the employee's new position be?",
+                name: "newRole"
             }
         ])
+        .then((answers) => {
+            //console.log(answers.employeeName);
+            
+            //  pool.query('UPDATE employees (employee_role) VALUES ($1)', [answers.newRole])
+        console.log('Data Inserted');     
+    });
+        
     }
+    
 });
     
 }
 init();
-
-
-
 
